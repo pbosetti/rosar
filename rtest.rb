c956f783ab84d0346ca4cb3c5ffc047b33a13b40
Paolo Bosetti <paolo.bosetti@me.com>
2009-03-19 18:14:21 +0100

    first commit

---
+++ rtest.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+#
+# Created by Paolo Bosetti on 2008-05-27.
+# Copyright (c) 2008 University of Trento. All rights 
+# reserved.
+
+require 'rosar'
+
+r = ROSAR.instance
+
+df = {
+  :x => (0..4).to_a,
+  :y => [7,2,5.5,8,9,10]
+}
+cols = %w(red green blue darkred darkgreen darkblue)
+r.transfer :p=>(0...100).to_a
+r.plot :x=>:p, :y=>"p^2", :typ=>"'l'"
+2.upto 7 do |i|
+  r.lines :x=>:p, :y=>"#{i}*p^2", :col=>"'#{cols[i-2]}'"
+end
+sleep(2)
+r.data_frame :df, df
+r.attach :df
+r.plot :x=>:x, :y=>"y/2", :typ=>"'b'", :col=>"'darkred'", :xlab=>"'Time (s)'"
+r.grid
+r.abline :h=>[2.5,3.5]
+r.abline "a=0, b=1"
+r.detach :df
